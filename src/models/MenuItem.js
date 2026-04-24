cb442c2fc45dc5b1601dcfd4099a2268f0c88cef
Sachin V S <vs1sachi12@gmail.com>
2026-04-24 11:13:58 +0530

    menuitem model

---
+++ src/models/MenuItem.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["starter", "main_course", "dessert", "beverage", "snacks"],
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("MenuItem", menuItemSchema);
