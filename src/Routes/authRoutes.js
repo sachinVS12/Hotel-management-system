1727fbb246a5159be9e8f73c1c5552ecbd5c3125
Sachin V S <vs1sachi12@gmail.com>
2026-04-27 10:26:02 +0530

    authroutes

---
+++ src/Routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const { authenticate } = require("../middleware/authMiddleware");
+
+router.post("/login", authController.login);
+router.post("/logout", authenticate, authController.logout);
+router.get("/me", authenticate, authController.getCurrentUser);
+
+module.exports = router;
